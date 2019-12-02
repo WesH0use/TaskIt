a29d1abf22973525578993ebae8cf9711cf60854
Wes House <wesevan7@gmail.com>
2019-12-02 11:18:52 +0100

    Initial Commit

---
+++ TaskIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskIt
+//
+//  Created by Wesley House on 12/2/19.
+//  Copyright © 2019 Wesley House. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
