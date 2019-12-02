960b3ef61b77b0806c21a05bb6ffb45635681ad3
Wes House <wesevan7@gmail.com>
2019-12-02 21:01:14 +0100

    Add new tableViewController with static cell, a Done bar button and a Cancel bar button. Create NewItemViewController.

---
+++ TaskIt/Model/NewItemTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NewItemTableViewController.swift
+//  TaskIt
+//
+//  Created by Wesley House on 12/2/19.
+//  Copyright © 2019 Wesley House. All rights reserved.
+//
+
+import UIKit
+
+class NewItemTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
