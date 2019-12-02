925c3f47913ccb96d0cdd8fb0dca2c61830e2563
Wes House <wesevan7@gmail.com>
2019-12-02 21:15:06 +0100

    Tester functionality to Done and Cancel Buttons

--- TaskIt/Model/NewItemTableViewController.swift
+++ TaskIt/Model/NewItemTableViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class NewItemTableViewController: UITableViewController {
 
+    
+    @IBAction func doneButtonPressed(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    
+    @IBAction func cancelButtonPressed(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
