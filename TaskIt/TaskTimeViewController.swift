af36a961d235d812a8e02efb255f672398d5870a
Wes House <wesevan7@gmail.com>
2019-12-02 12:09:05 +0100

    Add logic for selecting and deselecting the accessory checkmark type

--- TaskIt/TaskTimeViewController.swift
+++ TaskIt/TaskTimeViewController.swift
@@ -16,19 +16,32 @@ class TaskTimeViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 3
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "taskItemCell", for: indexPath)
         if let label = cell.viewWithTag(1000) as? UILabel {
-            if indexPath.row == 0 {
+            if indexPath.row % 3 == 0 {
                 label.text = "My first cell"
-            } else {
-                label.text = "My other cells"
+            } else if indexPath.row % 2 == 0 {
+                label.text = "Here's another cell"
+            } else if indexPath.row % 1 == 0 {
+                label.text = "And another"
             }
         }
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            if cell.accessoryType == .none {
+                cell.accessoryType = .checkmark
+            } else if cell.accessoryType == .checkmark {
+                cell.accessoryType = .none
+            }
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
 }
