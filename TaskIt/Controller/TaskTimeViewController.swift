c4a422a4c247eee141c61106e825d0701218b8da
Wes House <wesevan7@gmail.com>
2019-12-02 15:08:28 +0100

    Embed in Navigation Controller and set large titles for navigation bar.

--- TaskIt/Controller/TaskTimeViewController.swift
+++ TaskIt/Controller/TaskTimeViewController.swift
@@ -23,6 +23,7 @@ class TaskTimeViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = true
         // Do any additional setup after loading the view.
     }
 
