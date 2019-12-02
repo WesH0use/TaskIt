096f8da2277ffda131f805be754a8d232c652eef
Wes House <wesevan7@gmail.com>
2019-12-02 20:45:39 +0100

    Add swipe to delete functionality

--- TaskIt/Controller/TaskTimeViewController.swift
+++ TaskIt/Controller/TaskTimeViewController.swift
@@ -40,6 +40,13 @@ class TaskTimeViewController: UITableViewController {
     }
     
     
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        taskList.taskListArray.remove(at: indexPath.row)
+        let indexPaths = [indexPath]
+        tableView.deleteRows(at: indexPaths, with: .fade)
+    }
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "taskItemCell", for: indexPath)
         let item = taskList.taskListArray[indexPath.row]
