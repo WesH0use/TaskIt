f0f559dc2afa005c74f18a05191c570c146a2669
Wes House <wesevan7@gmail.com>
2019-12-04 20:57:52 +0100

    Add NewItemViewControllerDelegate (func newItemViewControllerDidCancel and func newItemViewControllerDidDone) and a delegate variable for the NewItemViewControllerDelegate

--- TaskIt/Controller/NewItemTableViewController.swift
+++ TaskIt/Controller/NewItemTableViewController.swift
@@ -8,9 +8,21 @@
 
 import UIKit
 
-class NewItemTableViewController: UITableViewController {
 
+protocol NewItemViewControllerDelegate : class {
+    func newItemViewControllerDidCancel(_ controller: NewItemTableViewController)
+    func newItemViewControllerDidDone(_ controller: NewItemTableViewController, didFinishDone item: TaskListItem)
+}
+
+
+class NewItemTableViewController: UITableViewController {
+    
+    weak var delegate : NewItemViewControllerDelegate?
+    
     @IBOutlet weak var usersItemInput: UITextField!
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var doneButton: UIBarButtonItem!
+    
     
     @IBAction func doneButtonPressed(_ sender: Any) {
         navigationController?.popViewController(animated: true)
@@ -42,6 +54,18 @@ extension NewItemTableViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         usersItemInput.resignFirstResponder()
         return false
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let oldText = usersItemInput.text,
+            let stringRange = Range(range, in: oldText) else {return false}
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        if newText.isEmpty {
+            doneButton.isEnabled = false
+        } else {
+            doneButton.isEnabled = true
+        }
+        return true
     }
     
 }
